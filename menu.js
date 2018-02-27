c367cfd5012f82cfd62930295d237d1fc92a4b0b
SYOfficial <stephen6678@hotmail.com>
2018-02-26 23:04:42 -0800

    Revert "added everything"
    This reverts commit 199b0301f83a19ad837b98366fcf16961f1e0a31.

--- menu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("downimg").addEventListener("click", function(){
-    document.getElementById("menu").style.top = "0px";
-    document.getElementById("menu").style.transition = "1s";
-});

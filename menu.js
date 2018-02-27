199b0301f83a19ad837b98366fcf16961f1e0a31
SYOfficial <stephen6678@hotmail.com>
2018-02-26 22:45:55 -0800

    added everything

---
+++ menu.js
@@ -0,0 +1,4 @@
+document.getElementById("downimg").addEventListener("click", function(){
+    document.getElementById("menu").style.top = "0px";
+    document.getElementById("menu").style.transition = "1s";
+});
